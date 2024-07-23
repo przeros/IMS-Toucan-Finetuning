1be143138903d118cc86988f314085c0fae0a6ab
Wiktor Krasinski <wiktor.krasinski@asseco.pl>
2024-07-23 21:24:46 +0200

    Comment out heavy checkpoints saving

--- TrainingInterfaces/Text_to_Spectrogram/ToucanTTS/toucantts_train_loop.py
+++ TrainingInterfaces/Text_to_Spectrogram/ToucanTTS/toucantts_train_loop.py
@@ -165,14 +165,14 @@ def train_loop(net,
         default_embedding = style_embedding_function(
             batch_of_spectrograms=train_dataset[0][2].unsqueeze(0).to(device),
             batch_of_spectrogram_lengths=train_dataset[0][3].unsqueeze(0).to(device)).squeeze()
-        torch.save({
-            "model"       : net.state_dict(),
-            "optimizer"   : optimizer.state_dict(),
-            "step_counter": step_counter,
-            "scheduler"   : scheduler.state_dict(),
-            "default_emb" : default_embedding,
-        }, os.path.join(save_directory, "checkpoint_{}.pt".format(step_counter)))
-        delete_old_checkpoints(save_directory, keep=1)
+        # torch.save({
+        #     "model"       : net.state_dict(),
+        #     "optimizer"   : optimizer.state_dict(),
+        #     "step_counter": step_counter,
+        #     "scheduler"   : scheduler.state_dict(),
+        #     "default_emb" : default_embedding,
+        # }, os.path.join(save_directory, "checkpoint_{}.pt".format(step_counter)))
+        # delete_old_checkpoints(save_directory, keep=1)
 
         print("\nEpoch:                  {}".format(epoch))
         print("Time elapsed:           {} Minutes".format(round((time.time() - start_time) / 60)))
