939b77d0fc05933f7c4d1c4ef07422ae74d56688
przeros <s180150@student.pg.edu.pl>
2024-08-11 12:49:20 +0200

    fix run bash script

--- run_script.sh
+++ run_script.sh
@@ -7,7 +7,7 @@ git pull
 # 2. Recursively delete all files except 'dataset' from the directory '../Dataset'
 echo "Recursively cleaning up ../Dataset directory..."
 find ../Dataset -type f ! -name 'dataset' -delete
-find ../Dataset -type d ! -name 'dataset' -exec rm -rf {} +
+find ../Dataset -type d ! -name 'dataset' ! -name 'Dataset' -exec rm -rf {} +
 
 # 3. Run the Python script with the specified parameters
 echo "Running the training pipeline..."
