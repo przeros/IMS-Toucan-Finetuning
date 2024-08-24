a8c11b85ac45e5bed35f137fe066d9555738145a
Wiktor Krasinski <wiktor.krasinski@asseco.pl>
2024-08-24 21:37:50 +0200

    Develop web app

--- Frontend/synthesis-web-app/src/App.js
+++ Frontend/synthesis-web-app/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [audioUrl, setAudioUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSend = async () => {
+    if (text.trim() === '') return;
+    
+    setLoading(true);
+    try {
+      const response = await axios.post('http://localhost:8000/speak', { text }, {
+        responseType: 'blob', // Expecting a binary response
+      });
+      const audioBlob = new Blob([response.data], { type: 'audio/mpeg' });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioUrl(audioUrl);
+    } catch (error) {
+      console.error('Error generating audio:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        Wygeneruj audio z tekstu, który wprowadzisz!
+      </div>
+      <div className="content">
+        <div className="text-box">
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Wprowadź tu tekst..."
+          />
+        </div>
+        <button className="send-button" onClick={handleSend}>
+          Generuj
+        </button>
+        <div className="audio-container">
+          {loading ? (
+            <div className="spinner"></div>
+          ) : (
+            audioUrl && <audio className="audio-player" controls src={audioUrl} />
+          )}
+        </div>
+      </div>
+      <footer className="footer">
+        Wykonane przez Wiktora Krasińskiego i Przemysława Roślenia<br/> praca magisterska 2024
+      </footer>
     </div>
   );
 }
